d1c5fd8bae007211694bb9fa2c95d14dd7a6ba3f
John McBride <jpmmcbride@gmail.com>
2018-05-19 11:03:19 -0600

    Initial commit. Basic skeleton

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # RecoverME
 CS 361 - Basic implementation and skeleton for the RecoverME application
+
+## Set up local dev enviroment:
+1. Pull from this githup repo
+2. Install the local node modules using `npm install`
+- This will install the needed view and engine modules to run the app
+3. Start the application with `node server.js`
+- You may now navigate to a locally running version of the application at `localhost:3000`
+
+
+## Basic skeleton of the App:
+
+1. `server.js` is the entry point for the node app. Any route handlers or various server logic should go here.
+2. `public` holds the various static resources for the app (CSS, images, etc). To access these resources, ensure that the full file path is specified (see main.handlebars for an example)
+3. `views` holds the various handlebars views that can be dynamically generated
+
+
+## To add a node module:
+1. Install the module using `npm install <MODULE NAME>`
+2. `require` the node module in server.js
+3. Use the various modules components
---
+++ server.js
@@ -0,0 +1,55 @@
+// CS 361 - Software engineering
+// RecoverME 
+// ---------
+// Description: Node JS entry point
+
+// Include the needed middleware
+var express = require('express');
+var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+// Build the application
+var app = express();
+
+// For the path to the public folder in the express fild tree
+app.use(express.static('public'));
+
+// set up the application
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', process.env.PORT || 3000); // TODO - Change on engineering server
+
+// set up the body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// ---------------
+// The GET request for the web app
+// ---------------
+app.get('/', function(req, res){
+    // Send to the home page
+    res.render('home');
+});
+
+// -------------------------
+// --- Server functions ----
+// -------------------------
+
+// The route (404) error handler
+app.use(function(req, res){
+    res.status(404);
+    res.render('404');
+});
+
+// The server (500) error handler
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+// Start the application on the specified port (port 3000 for local development)
+app.listen(app.get('port'), function(){
+    console.log('Express started on port:' + app.get('port'));
+});
